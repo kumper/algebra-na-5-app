ebff55c4a7f894ab8c4a9c152da6be35d178a086
Kamil Parzych (BD/MSA-IPM9) <kamil.parzych@pl.bosch.com>
2026-03-28 00:18:38 +0100

    questions database

--- src/app/services/question.service.ts
+++ src/app/services/question.service.ts
@@ -1,15 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable, shareReplay, map } from 'rxjs';
+import { forkJoin, Observable, shareReplay, map } from 'rxjs';
 import { Question } from '../models/question.model';
 
 @Injectable({ providedIn: 'root' })
 export class QuestionService {
   readonly #http = inject(HttpClient);
 
-  readonly #questions$: Observable<Question[]> = this.#http
-    .get<Question[]>('assets/data/questions.json')
-    .pipe(shareReplay(1));
+  readonly #questions$: Observable<Question[]> = forkJoin(
+    [1, 2, 3, 4, 5].map((level) =>
+      this.#http.get<Question[]>(`assets/data/questions-${level}.json`)
+    )
+  ).pipe(
+    map((arrays) => arrays.flat()),
+    shareReplay(1)
+  );
 
   getAll(): Observable<Question[]> {
     return this.#questions$;
@@ -20,5 +25,12 @@ export class QuestionService {
       map((questions) => questions.find((q) => q.id === id))
     );
   }
+
+  getByDifficulty(difficulty: 1 | 2 | 3 | 4 | 5): Observable<Question[]> {
+    return this.#questions$.pipe(
+      map((questions) => questions.filter((q) => q.difficulty === difficulty))
+    );
+  }
 }
+
 
